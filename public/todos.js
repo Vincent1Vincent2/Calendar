d3186908dde8985df1f1d478252f9c530ab52f9a
Daniela <danielapatriciabystrom@gmail.com>
2023-12-08 09:28:36 +0100

    Updated editTodoTitle-function

--- public/todos.js
+++ public/todos.js
@@ -18,7 +18,7 @@ function createTodoStructure() {
     const editTodoBtn = document.createElement('button');
 
     todoContainer.setAttribute('data-cy', 'todo-list');
-    todoTitleElement.classList.add = 'todoTitle';
+    todoTitleElement.classList.add('todoTitle');
     todoDateElement.classList.add('todoDate');
     todoBtnContainer.classList.add('todoBtns');
 
@@ -28,38 +28,38 @@ function createTodoStructure() {
     editTodoBtn.setAttribute('data-cy', 'edit-todo-button');
     editTodoBtn.textContent = 'Edit';
 
-    let title = todos[i]['title'];
-    let date = todos[i]['date'];
+    let todo = todos[i];
 
-    todoTitleElement.textContent = title;
-    todoDateElement.textContent = date;
+    todoTitleElement.textContent = todo.title;
+    todoDateElement.textContent = todo.date;
 
     newTodoContainer.appendChild(todoContainer);
     todoContainer.appendChild(todoTitleElement);
     todoContainer.appendChild(todoDateElement);
-    todoContainer.appendChild(todoBtnContainer);
+    todoTitleElement.appendChild(todoBtnContainer);
     todoBtnContainer.appendChild(deleteTodoBtn);
-    todoTitleElement.appendChild(editTodoBtn);
+    todoBtnContainer.appendChild(editTodoBtn);
 
     deleteTodoBtn.addEventListener('click', function () {
       todos.splice(i, 1);
       createTodoStructure();
+
+      generateCalendar(currentYear, currentMonth);
+
     });
     editTodoBtn.addEventListener('click', function () {
-        editTodoTitle(i, todoTitleElement);
+      editTodoTitle(todo);
     });
-  }
-}
 
-// Funktion redigera titeln todo
-function editTodoTitle(index, titleElement) {
-    const newTitle = prompt('Edit Todo Title:', titleElement.textContent); // Pop-up prompt med ny titel, använder den nuvarande titels dom standardvärd
-    // Kontrollera om användaren klickat på cancel eller om ny titel är empty
-    if (newTitle !== null && newTitle.trim() !== '') {
-      titleElement.textContent = newTitle; // Uppdaterar textinnehållet i todo med nya titeln
-      todos[index].title = newTitle; // Uppdaterar arrayen med nya titeln på rätt plats
+    function editTodoTitle(todo) {
+      const newTitle = prompt('Enter the new title for the todo:', todo.title);
+      if (newTitle !== null) {
+        todo.title = newTitle;
+        createTodoStructure();
+      }
     }
   }
+}
 
 addTodoBtn.addEventListener('click', function () {
   addTodo();
@@ -77,76 +77,7 @@ function addTodo() {
   todos.push(newTodo);
   createTodoStructure();
 
-//   todoTitleInput.value = '';
+  generateCalendar(currentYear, currentMonth);
+
 
 }
-
-// Add an event listener to the form to handle todo submission
-/* form.addEventListener('submit', event => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  // Get the todo-task and selected date form input-field
-
-  // Create a new task element as todo
-  const taskElement = document.createElement('div');
-  taskElement.classList.add('task');
-
-  // Create a content element to hold the task details
-  const taskContentElement = document.createElement('div');
-  taskContentElement.classList.add('content');
-
-  taskElement.appendChild(taskContentElement); // Append the content element to the task element
-
-  // Create an input element to display the task text
-  const taskInputElement = document.createElement('input');
-  taskInputElement.classList.add('text');
-  taskInputElement.type = 'text';
-  taskInputElement.value = task + ' - ' + selectedDate; // Combines task and date
-  taskInputElement.setAttribute('readonly', 'readonly'); // Makes the input readonly
-
-  taskContentElement.appendChild(taskInputElement); // Append the input element to the content element
-
-  // Create action element to hold edit and delete-buttons
-  const taskActionsElement = document.createElement('div');
-  taskActionsElement.classList.add('actions');
-
-  // Create an edit-button
-  const taskEditElement = document.createElement('button');
-  taskEditElement.classList.add('edit');
-  taskEditElement.setAttribute('data-cy', 'edit-todo-button');
-  taskEditElement.innerHTML = 'Edit';
-
-  // Create an delete-button
-  const taskDeleteElement = document.createElement('button');
-  taskDeleteElement.classList.add('delete');
-  taskDeleteElement.setAttribute('data-cy', 'delete-todo-button');
-  taskDeleteElement.innerHTML = 'Delete';
-
-  // Append edit and delete buttons to the actions element
-  taskActionsElement.appendChild(taskEditElement);
-  taskActionsElement.appendChild(taskDeleteElement);
-
-  // Append the actions element to the task element
-  taskElement.appendChild(taskActionsElement);
-
-  // Append the task element to the list container
-  listElement.appendChild(taskElement);
-
-  // Clears the input field
-  todoTitleInput.value = '';
-
-  // Add event listener for edit button click
-  taskEditElement.addEventListener('click', () => {
-    // Toggle between editing and saving mode
-    if (taskEditElement.innerText.toLowerCase() == 'edit') {
-      taskInputElement.removeAttribute('readonly'); // Allows editing
-      taskInputElement.focus(); // Set focus to the input
-      taskEditElement.innerText = 'Save'; // Change button text to "Save"
-    } else {
-      taskInputElement.setAttribute('readonly', 'readonly'); // Make input readonly
-      taskEditElement.innerText = 'Edit'; // Change button text back to "Edit"
-    }
-  });
-
-
- */
